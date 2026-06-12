b690b345de4629b4105bd700ddde6b7862981d2b
elisabichiappini <elisabichiappini@gmail.com>
2026-06-12 12:07:21 +0200

    feat: new html

--- src/assets/components/Post/Post.jsx
+++ src/assets/components/Post/Post.jsx
@@ -1,17 +1,21 @@
-
-import PostImage from './PostImage';
-import PostText from './PostText';
-
+import postStyle from './Post.module.css';
+import imgPost from '../../../img/lollipop.webp';
+import Button from '../Button/Button';
 const Post = () => {
     return (
         <>
             <div className="post">
-                <PostImage />
-                <PostText
-                    title="My First Post"
-                    description="This is my first post!"
-                    like={3}
-                />
+                <figure >
+                    <img src={imgPost} alt="Post image" className={postStyle['post-box']} />
+                </figure>
+                <div className={postStyle.post}>
+                    <h2 className={postStyle['pt-8']}>title</h2>
+                    <p className={postStyle['pt-8']}>
+                        lorem ipsum dolor sit amet consectetur adipisicing elit. Velit animi unde quasi enim non esse ratione voluptas voluptate, officiis veritatis magni blanditiis possimus nobis cum id inventore corporis deserunt hic.
+                    </p>
+                    <h6> Title</h6>
+                    <Button />
+                </div>
             </div>
         </>
     )
