b59eb0dfd88b04aeedcf36f053b7750c461e131e
elisabichiappini <elisabichiappini@gmail.com>
2026-06-12 14:08:53 +0200

    feat: props

--- src/assets/components/Main/Main.jsx
+++ src/assets/components/Main/Main.jsx
@@ -1,10 +1,21 @@
 import Post from '../Post/Post';
+import { posts } from '../../utils/posts';
+
 const Main = () => {
     return (
         <main className="d-flex flex-column align-items-center justify-content-center gap-4 flex-wrap">
-            <Post/>   
+            {
+                posts.map(p => (
+                    p.published === true && 
+                     <Post key={`post-${p.id}`} 
+                    title={p.title} 
+                    description={p.content} 
+                    imgSrc={p.image} 
+                    />
+                ))
+            }
         </main>
     )
 }
 
-export default Main;
+export default Main; 
--- src/assets/components/Post/Post.jsx
+++ src/assets/components/Post/Post.jsx
@@ -1,19 +1,21 @@
 import postStyle from './Post.module.css';
-import imgPost from '../../../img/lollipop.webp';
 import Button from '../Button/Button';
-const Post = () => {
+
+
+const Post = ({ title, description, imgSrc}) => {
     return (
         <>
-            <div className="post">
+            <div className="post" >
+               
                 <figure >
-                    <img src={imgPost} alt="Post image" className={postStyle['post-box']} />
+                    <img src={imgSrc} alt="Post image" className={postStyle['post-box']} />
                 </figure>
                 <div className={postStyle.post}>
-                    <h2 className={postStyle['pt-8']}>title</h2>
+                    <h2 className={postStyle['pt-8']}>{title}</h2>
                     <p className={postStyle['pt-8']}>
-                        lorem ipsum dolor sit amet consectetur adipisicing elit. Velit animi unde quasi enim non esse ratione voluptas voluptate, officiis veritatis magni blanditiis possimus nobis cum id inventore corporis deserunt hic.
+                        {description}
                     </p>
-                    <h6> Title</h6>
+  
                     <Button />
                 </div>
             </div>
