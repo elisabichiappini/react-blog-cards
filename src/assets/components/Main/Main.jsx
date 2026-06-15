a5e71f39b43f52aa236cf2d9fd07d2a7c5cabe2f
elisabichiappini <elisabichiappini@gmail.com>
2026-06-15 12:15:53 +0200

    feat: layout

--- src/assets/components/Main/Main.jsx
+++ src/assets/components/Main/Main.jsx
@@ -1,22 +1,43 @@
 import Post from '../Post/Post';
 import { posts } from '../../utils/posts';
 
-const Main = () => {
-    return (
-        <main className="d-flex flex-column align-items-center justify-content-center gap-4 flex-wrap">
-            {
-                posts.map(p => (
-                    p.published === true && 
-                     <Post key={`post-${p.id}`} 
-                    title={p.title} 
-                    description={p.content} 
-                    imgSrc={p.image} 
-                    tags={p.tags}
-                    />
-                ))
+export default function Main() {
+    const uniqueTags = posts.reduce((acc, post) => {
+        post.tags.forEach((tag) => {
+            if (!acc.includes(tag)) {
+                acc.push(tag);
             }
-        </main>
-    )
-}
+        });
 
-export default Main; 
+        return acc;
+    }, []);
+
+    return (
+        <main className="info">
+            <h3 >All tags</h3>
+            <div className="tags">
+                {uniqueTags.map((tag, index) => (
+                    <span
+                        key={`allTag-${index}`}
+                        className={`tag ${tag}`}
+                    >
+                        {tag}
+                    </span>
+                ))}
+            </div>
+
+            {posts.map(
+                (p) =>
+                    p.published === true && (
+                        <Post
+                            key={`post-${p.id}`}
+                            title={p.title}
+                            description={p.content}
+                            imgSrc={p.image}
+                            tags={p.tags}
+                        />
+                    )
+            )}
+        </main>
+    );
+}
